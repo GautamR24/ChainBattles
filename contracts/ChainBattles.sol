62fc9a4e7624181bd9abe560497994d8b88a157a
GautamR24 <rawatgautam24@protonmail.com>
2022-10-27 13:41:19 +0530

    Changed simple uint mapping to uint=>struct

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -12,11 +12,22 @@ contract ChainBattles is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    struct traits {
+        uint256 Level;
+        uint256 Speed;
+        uint256 Strength;
+        uint256 Life;
+    }
+
+    mapping(uint256 => traits) public tokenIdToLevels;
 
     constructor() ERC721("Chain Battles", "CBTLS") {}
 
-    function generateCharacter(uint256 tokenId) public view returns (string memory) {
+    function generateCharacter(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
@@ -27,6 +38,12 @@ contract ChainBattles is ERC721URIStorage {
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
             "Levels: ",
             getLevels(tokenId),
+            "Speed: ",
+            getSpeed(tokenId),
+            "Strength: ",
+            getStrength(tokenId),
+            "Life: ",
+            getLife(tokenId),
             "</text>",
             "</svg>"
         );
@@ -40,8 +57,23 @@ contract ChainBattles is ERC721URIStorage {
     }
 
     function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
+        uint256 levels = tokenIdToLevels[tokenId].Level;
         return levels.toString(); //we are converting to strings because the content of abi.encode must always resolve to a string
+    }
+
+    function getSpeed(uint256 tokenId) public view returns (string memory) {
+        uint256 speed = tokenIdToLevels[tokenId].Speed;
+        return speed.toString(); //we are converting to strings because the content of abi.encode must always resolve to a string
+    }
+
+    function getStrength(uint256 tokenId) public view returns (string memory) {
+        uint256 strength = tokenIdToLevels[tokenId].Strength;
+        return strength.toString(); //we are converting to strings because the content of abi.encode must always resolve to a string
+    }
+
+    function getLife(uint256 tokenId) public view returns (string memory) {
+        uint256 life = tokenIdToLevels[tokenId].Life;
+        return life.toString(); //we are converting to strings because the content of abi.encode must always resolve to a string
     }
 
     /*
@@ -72,7 +104,10 @@ contract ChainBattles is ERC721URIStorage {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        tokenIdToLevels[newItemId].Level = 0;
+        tokenIdToLevels[newItemId].Speed = 0;
+        tokenIdToLevels[newItemId].Strength = 0;
+        tokenIdToLevels[newItemId].Life = 0;
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
@@ -82,8 +117,12 @@ contract ChainBattles is ERC721URIStorage {
             ownerOf(tokenId) == msg.sender,
             "You must own this token to train it"
         );
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        uint256 currentLevel = tokenIdToLevels[tokenId].Level;
+        tokenIdToLevels[tokenId].Level = currentLevel + 1;
+        tokenIdToLevels[tokenId].Level = currentLevel + 1;
+        tokenIdToLevels[tokenId].Speed = currentLevel + 1;
+        tokenIdToLevels[tokenId].Strength = currentLevel + 1;
+        tokenIdToLevels[tokenId].Life = currentLevel + 1;
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
